03e737031bf7251280bf337e74c551c12b668245
Birappa Goudanavar <birappa001@gmail.com>
2024-03-26 10:33:54 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from src.amfi_scraper import amfi_scraper_main
 from src.irdai_scraper import irdai_scraper_main
 
 def main():
-    # amfi_scraper_main()
+    amfi_scraper_main()
     irdai_scraper_main()
