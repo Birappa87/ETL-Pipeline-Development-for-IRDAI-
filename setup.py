2abe7efb0167260389c9ef3c081a90e9f80416d8
Birappa Goudanavar <birappa001@gmail.com>
2024-03-27 20:27:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-from setuptools import setup, find_packages
-from setuptools.command.install import install as _install
+from src.main import main
 
-# Custom install command to run 'playwright install'
-# class InstallCommand(_install):
-#     def run(self):
-#         _install.run(self)
-#         # Run 'playwright install' after installation
-#         import subprocess
-#         subprocess.run(['playwright', 'install'], check=True)
-
-# Read requirements from requirements.txt file
-with open('requirements.txt', 'r', encoding='utf-16') as f:
-    requirements = f.read().splitlines()
-
-setup(
-    name='Irdai and afmi',
-    version='1.0',
-    packages=find_packages(),
-    install_requires=requirements,
-    entry_points={
-        'console_scripts': [
-            'amfi_irdai_bot_runner = src.main:main',  # Adjust the module and function name accordingly
-        ],
-    },
-    # cmdclass={
-    #     'install': InstallCommand,
-    # }
-)
+main()
